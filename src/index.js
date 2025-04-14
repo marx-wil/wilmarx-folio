55db882ada0c492920bf2770bd6216585935495b
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-14 15:43:15 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
 import MyRoutes from "./routes";
-import { BrowserRouter as Router } from 'react-router-dom';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { BrowserRouter as Router } from "react-router-dom";
+import "./pages/default/poppins.scss";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<ChakraProvider>
+  <ChakraProvider>
     <React.StrictMode>
-        <Router>
-            <MyRoutes />
-        </Router>
+      <Router>
+        <MyRoutes />
+      </Router>
     </React.StrictMode>
-</ChakraProvider>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
