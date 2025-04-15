8af2d0796e96e7f7a6914230e085165a912a7884
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-15 09:58:42 +0800

    feat: added the pages with basic content

---
+++ src/pages/workHistory/index.js
@@ -0,0 +1,14 @@
+import HeroSectionWithSocialLinks from "../../components/heroSection";
+const WorkHistory = () => {
+  return (
+    <HeroSectionWithSocialLinks
+      heroText="Work History"
+      footerHead="Would you like to"
+      footerBody="Work with me?"
+       footerSub="Click to view"
+      navigateTo="/contact"
+    />
+  );
+};
+
+export default WorkHistory;
