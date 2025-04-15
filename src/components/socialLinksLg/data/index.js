9d201ae8953d1ab17962284e30d13400b280e939
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-15 10:55:01 +0800

    feat: easy to maintain social links data

---
+++ src/components/socialLinksLg/data/index.js
@@ -0,0 +1,26 @@
+import { FaGithub, FaInstagram, FaFacebook, FaLinkedin } from "react-icons/fa6";
+
+const socialLinksLg = [
+  {
+    name: "Github",
+    icon: FaGithub,
+    link: "https://github.com/yourusername",
+  },
+  {
+    name: "Instagram",
+    icon: FaInstagram,
+    link: "https://www.instagram.com/yourusername",
+  },
+  {
+    name: "Facebook",
+    icon: FaFacebook,
+    link: "https://www.facebook.com/yourusername",
+  },
+  {
+    name: "Linkedin",
+    icon: FaLinkedin,
+    link: "https://www.linkedin.com/in/yourusername",
+  },
+];
+
+export default socialLinksLg;
