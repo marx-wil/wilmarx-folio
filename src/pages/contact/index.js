09c17be0e9d180ec4b1c2f156be1a4441e80e2ee
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-15 09:58:21 +0800

    feat: added the pages with basic content

---
+++ src/pages/contact/index.js
@@ -0,0 +1,14 @@
+import HeroSectionWithSocialLinks from "../../components/heroSection";
+const Contact = () => {
+  return (
+    <HeroSectionWithSocialLinks
+      heroText="Contact"
+      footerHead="Go back to"
+      footerBody="Root of Website"
+      footerSub="Click to go back"
+      navigateTo="/"
+    />
+  );
+};
+
+export default Contact;
