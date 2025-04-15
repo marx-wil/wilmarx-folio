b2df10dd655541bedb069dfdd4e0f791562127b9
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-15 10:47:23 +0800

    refactor(about): Convert About page to HeroSection HOC

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,8 +1,43 @@
-import HeroSectionWithSocialLinks from "../../components/heroSection";
+import { Text, useColorModeValue, VStack } from "@chakra-ui/react";
+import HeroSection from "../../components/heroSection";
+
+// Create a content component for the middle section
+const AboutContent = () => {
+  return (
+    <VStack align="flex-start" spacing={4}>
+      <Text
+        mb={0}
+        fontSize={{ base: "2xl", md: "3xl", lg: "3xl" }}
+        fontWeight="bold"
+        letterSpacing="0.1rem"
+        lineHeight="1"
+        className="poppins"
+        color={useColorModeValue("#757575", "#A0A0A0")}
+        textTransform="uppercase"
+      >
+        Who I Am
+      </Text>
+      <Text
+        fontSize={{ base: "5xl", md: "7xl", lg: "8xl" }}
+        fontWeight="bold"
+        letterSpacing="tight"
+        lineHeight="1"
+        className="poppins"
+        color={useColorModeValue("#4f4f4f", "#F7F8FA")}
+        textTransform="uppercase"
+      >
+        About Me
+      </Text>
+    </VStack>
+  );
+};
+
+// Create a new component with the HeroSection HOC
+const HeroWithContent = HeroSection(AboutContent);
+
 const About = () => {
   return (
-    <HeroSectionWithSocialLinks
-      heroText="About Me"
+    <HeroWithContent
       footerHead="Projects I've "
       footerBody="Worked on"
       footerSub="Click to view"
