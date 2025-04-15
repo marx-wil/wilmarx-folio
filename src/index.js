7af36208999ea11aa2209826ebd9a07611fc0ab0
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-15 08:05:13 +0800

    chore: update import location

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import MyRoutes from "./routes";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./pages/default/poppins.scss";
+import "./design/sass/poppins.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider>
