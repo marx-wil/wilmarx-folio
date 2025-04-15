c986eaa0abf7cf80bf2aaea0f52da33e73568ed4
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-15 09:58:31 +0800

    feat: added the pages with basic content

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,5 +1,14 @@
+import HeroSectionWithSocialLinks from "../../components/heroSection";
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <HeroSectionWithSocialLinks
+      heroText="Projects"
+      footerHead="A story about"
+      footerBody="My Growth"
+      footerSub="Click to view"
+      navigateTo="/work-history"
+    />
+  );
 };
 
 export default Projects;
