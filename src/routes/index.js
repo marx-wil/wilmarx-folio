e9c375fb933b1a9a06286b3380f6253675bf315a
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-15 09:32:22 +0800

    feat: added "about" page to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,17 @@ import MainLayout from "../layout/main";
 
 // pages
 import Default from '../pages/default';
+import About from '../pages/about';
 
 // HOC
 const DefaultView = MainLayout(Default);
+const AboutView = MainLayout(About);
+
 const Router = () => {
 return (
 <Routes>
     <Route path="/" element={<DefaultView />} />
+    <Route path="/about" element={<AboutView />} />
 </Routes>
 )
 }
