eb49828fabe55bfb14e7c92ae7ebbf279b118353
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-15 09:47:26 +0800

    chore: added projects page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,25 @@
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 // layout
 import MainLayout from "../layout/main";
 
 // pages
-import Default from '../pages/default';
-import About from '../pages/about';
+import Projects from "../pages/projects";
+import Default from "../pages/default";
+import About from "../pages/about";
 
 // HOC
 const DefaultView = MainLayout(Default);
 const AboutView = MainLayout(About);
+const ProjectsView = MainLayout(Projects);
 
 const Router = () => {
-return (
-<Routes>
-    <Route path="/" element={<DefaultView />} />
-    <Route path="/about" element={<AboutView />} />
-</Routes>
-)
-}
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultView />} />
+      <Route path="/about" element={<AboutView />} />
+      <Route path="/projects" element={<ProjectsView />} />
+    </Routes>
+  );
+};
 
 export default Router;
