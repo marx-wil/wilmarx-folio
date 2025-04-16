6a1bd34a0abedf109ce3882f8a569600c894ef78
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-16 16:01:58 +0800

    feat: the projects section

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,44 +1,65 @@
-import { Text, useColorModeValue, VStack } from "@chakra-ui/react";
+import { VStack, Grid, GridItem } from "@chakra-ui/react";
 import HeroSection from "../../components/heroSection";
+import TextWithImage from "../../components/TextWithImage";
+import dev_avatar from "../../assets/me/myphoto.jpg";
+import HeroHeader from "../../components/HeroHeader";
+import DentalManagementSystem from "../../assets/projects/dems.jpg";
+const ProjectsContent = () => {
+  const textContent = [
+    {
+      content:
+        "Project Yggdrasil Genesis is a robust Dental Management System meticulously crafted to optimize the operational workflows of dental clinics. ",
+    },
+    {
+      content:
+        "It encompasses a comprehensive suite of features designed to streamline patient management, appointment scheduling, treatment documentation, billing processes, and more.",
+      display: { base: "none", xl: "block" },
+    },
+    {
+      content:
+        "By leveraging modern technologies and frameworks, this system aims to enhance the efficiency, productivity, and patient care standards within dental practices.",
+      display: { base: "none", md: "block" },
+    },
+  ];
 
-// Create a content component for the middle section
-const ProjectContent = () => {
   return (
-    <VStack align="flex-start" spacing={4}>
-      <Text
-        mb={0}
-        fontSize={{ base: "2xl", md: "3xl", lg: "3xl" }}
-        fontWeight="bold"
-        letterSpacing="0.1rem"
-        lineHeight="1"
-        className="poppins"
-        color={useColorModeValue("#757575", "#A0A0A0")}
-        textTransform="uppercase"
+    <VStack
+      align="stretch"
+      spacing={{ base: 4, md: 8 }}
+      maxW="5xl"
+      w="full"
+      px={{ base: 4, md: 0 }}
+      position="relative"
+    >
+      <Grid
+        templateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}
+        gap={{ base: 6, md: 12 }}
+        w="full"
+        alignItems="center"
       >
-        My Works
-      </Text>
-      <Text
-        fontSize={{ base: "5xl", md: "7xl", lg: "8xl" }}
-        fontWeight="bold"
-        letterSpacing="tight"
-        lineHeight="1"
-        className="poppins"
-        color={useColorModeValue("#4f4f4f", "#F7F8FA")}
-        textTransform="uppercase"
-      >
-        Projects
-      </Text>
+        <GridItem>
+          <HeroHeader title="My Works" subtitle="Pro" highlightText="jects." />
+        </GridItem>
+
+        <GridItem alignSelf="center" position="relative">
+          <TextWithImage
+            textContent={textContent}
+            imageSrc={DentalManagementSystem}
+            footerTag="Dental Management System"
+          />
+        </GridItem>
+      </Grid>
     </VStack>
   );
 };
 
 // Create a new component with the HeroSection HOC
-const HeroWithContent = HeroSection(ProjectContent);
+const HeroWithContent = HeroSection(ProjectsContent);
 
 const Projects = () => {
   return (
     <HeroWithContent
-      footerHead="A story about"
+      footerHead="A story of "
       footerBody="My Growth"
       footerSub="Click to view"
       navigateTo="/work-history"
